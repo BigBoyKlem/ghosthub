ac403901ac6a13f319c64569014a3ab9bbcda8e9
BigBoyKlem <anthonybutts2016@gmail.com>
2020-12-22 21:01:34 -0500

    Added Website

---
+++ JS/Script.js
@@ -0,0 +1,17 @@
+(function() {
+
+const script = 'loadstring(game:HttpGet(\'cdn.websitename.com/script.lua\'))()';
+
+const copyButton = document.getElementById('copyScriptButton')
+
+
+copyButton.onclick = function() {
+    navigator.clipboard.writeText(script).then(doc => {
+        copyButton.innerHTML = 'Copied Script To Clipboard'
+        setTimeout(function() {
+            copyButton.innerHTML = 'Copy Script'
+        }, 2500)
+    });
+};
+
+})();
