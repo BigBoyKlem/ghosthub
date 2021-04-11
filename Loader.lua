3a97f4800c6899200c94ad308426935852ccc694
Anthony <anthonybutts2016@gmail.com>
2021-04-10 22:59:16 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,11 @@
 local gameList = loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/GameList.lua',true))()
 
 for i,v in pairs(gameList) do
-    if (v == game.PlaceId) then
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/' .. 'Games/' .. gameList[v] .. '.lua', true))
-    else
-        print(gameList)
-        print('No Game Found')
+    -- if (v == game.PlaceId) then
+    --     loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/' .. 'Games/' .. gameList[v] .. '.lua', true))
+    -- else
+    --     print(gameList)
+    --     print('No Game Found')
+    print (v)
     end
 end
