2c5ac5bf6984e965583fe338b0e3c3c909bc5fa3
Anthony <anthonybutts2016@gmail.com>
2021-04-10 22:53:05 -0400

    Added Loader Functionality

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,7 @@
-local gameList = loadstring(game:HttpGet("",true))()
+local gameList = loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/GameList.lua',true))()
+
+for i,v in pairs(gameList) do
+    if (v == game.PlaceId) then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/' .. 'Games/' .. gameList[v] .. '.lua', true))
+    end
+end
