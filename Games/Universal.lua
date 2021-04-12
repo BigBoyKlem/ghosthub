89a45786c0159e8c9c69505383ab5cdba74eb046
Anthony <anthonybutts2016@gmail.com>
2021-04-12 16:53:09 -0400

    Update Universal.lua

--- Games/Universal.lua
+++ Games/Universal.lua
@@ -1,10 +1,39 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wally2", true))()
 
 local playerWindow = library:CreateWindow("Player")
+local teleportWindow = library:CreateWindow("Teleport")
 local miscWindow = library:CreateWindow("Misc")
+
+local noClipToggled = false
+local playerlist = {}
+
+for i,v in pairs(game.Players:GetPlayers())do
+    if v ~= game.Players.LocalPlayer then
+        table.insert(playerlist,v.Name)
+    end
+end
 
 playerWindow:Slider("Walk Speed", {flag = "walkSpeedSlider", min = 16, max = 500})
 playerWindow:Slider("Jump Power", {flag = "jumpPowerSlider", min = 50, max = 500})
+playerWindow:Slider("FOV", {flag = "fovSlider", min = 70, max = 120})
+
+playerWindow:Bind("No Clip", {flag = "noClipBind", kbonly = true, default = Enum.KeyCode.E}, function()
+    
+    noClipToggled = not noClipToggled
+    
+    game:GetService('RunService').Stepped:Connect(function()
+        if (noClipToggled) then
+            game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+        end
+    end)
+end)
+
+local playerDropDown = teleportWindow:Dropdown("Player", {"playerDropDown", list = playerlist}, function(value) 
+    local playerPart = game.Players.LocalPlayer.Character.PrimaryPart
+    local targetPart = game.Players[value].Character.PrimaryPart
+
+    playerPart.CFrame = targetPart.CFrame
+end)
 
 miscWindow:Button("Rejoin Game", function()
     game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
@@ -16,7 +45,28 @@ end)
 
 while wait() do
     pcall(function()
+
+        -- Walk Speed / Jump Power
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = playerWindow.flags.walkSpeedSlider
         game.Players.LocalPlayer.Character.Humanoid.JumpPower = playerWindow.flags.jumpPowerSlider
+
+        -- FOV
+        game.Workspace.CurrentCamera.FieldOfView = playerWindow.flags.fovSlider
     end)
 end
+
+game.Players.PlayerAdded:Connect(function(player)
+    local name = player.Name
+    table.insert(playerlist,name)
+    playerDropDown:Refresh(playerlist)
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+    local name = player.Name
+    for i,v in pairs(playerlist)do
+        if v == name then  
+            table.remove(playerlist,i)
+        end
+    end
+    drop:Refresh(playerlist)
+end)
