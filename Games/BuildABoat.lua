f437d9cfaf0257004aa8a7221acb2b891c857e2f
Anthony <anthonybutts2016@gmail.com>
2021-04-12 13:36:00 -0400

    Updated Build a Boat / Added Discord Link

--- Games/BuildABoat.lua
+++ Games/BuildABoat.lua
@@ -1,12 +1,17 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wally2", true))()
 
 local playerWindow = library:CreateWindow("Player")
+local miscWindow = library:CreateWindow("Misc")
 
 playerWindow:Slider("Walk Speed", {flag = "walkSpeedSlider", min = 16, max = 500})
 playerWindow:Slider("Jump Power", {flag = "jumpPowerSlider", min = 50, max = 500})
 
-miscWindow:Button("Destroy UI", function()
-    game.CoreGui.ScreenGui:Destroy()
+miscWindow:Button("Rejoin Game", function()
+    game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
+end)
+
+miscWindow:Button("Get Discord Link", function()
+    setclipboard(loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/DiscordLink.lua',true))())
 end)
 
 while wait() do
