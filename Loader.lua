4206da38b6203f5897f794e635a5b78bf8fe3f23
Anthony <anthonybutts2016@gmail.com>
2021-04-17 23:38:39 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,11 @@
-if (game:GetService('CoreGui'):FindFirstChild("GHOSTHUB")) then return end
-
 local gameList = loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/GameList.lua',true))()
 
-print('Loading...')
+if (not game:GetService('CoreGui'):FindFirstChild("GHOSTGUI")) then
+    print('Loading...')
 
-if gameList[tostring(game.PlaceId)] then
-    loadstring(game:HttpGet(string.gsub('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/Games/' .. gameList[tostring(game.PlaceId)] .. '.lua', '%s+', '%%20'), true))()
-else
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/Games/Universal.lua', true))()
+    if gameList[tostring(game.PlaceId)] then
+        loadstring(game:HttpGet(string.gsub('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/Games/' .. gameList[tostring(game.PlaceId)] .. '.lua', '%s+', '%%20'), true))()
+    else
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/BigBoyKlem/GhostHub/master/Games/Universal.lua', true))()
+    end
 end
