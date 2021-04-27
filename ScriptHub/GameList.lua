e0efc0682298fc7b87fa5d744b726d9668b50f2e
Anthony <anthonybutts2016@gmail.com>
2021-04-27 11:11:25 -0400

    Added Bitcoin Miner

--- ScriptHub/GameList.lua
+++ ScriptHub/GameList.lua
@@ -2,5 +2,6 @@ return {
     ['5736409216'] = 'Mall Tycoon',
     ['4246487209'] = 'Boss Fighting Simulator',
     ['6094061470'] = 'Arcade Empire',
-    ['5450332716'] = 'Math Simulator'
+    ['5450332716'] = 'Math Simulator',
+    ['2629642516'] = 'Bitcoin Miner'
 }
