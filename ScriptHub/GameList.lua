bd3ca57c4f2eb6b9addb7603c66bb64a747066c0
Anthony <anthonybutts2016@gmail.com>
2021-04-30 20:34:20 -0400

    Added Thief Life Simulator

--- ScriptHub/GameList.lua
+++ ScriptHub/GameList.lua
@@ -3,5 +3,6 @@ return {
     ['4246487209'] = 'Boss Fighting Simulator',
     ['6094061470'] = 'Arcade Empire',
     ['5450332716'] = 'Math Simulator',
-    ['2629642516'] = 'Bitcoin Miner'
+    ['2629642516'] = 'Bitcoin Miner',
+    ['2693739238'] = 'Thief Life Simulator'
 }
