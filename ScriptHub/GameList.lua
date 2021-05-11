06ba416da3c7003952c5073f7d68d77a27ee1f4b
Anthony <anthonybutts2016@gmail.com>
2021-05-11 10:26:16 -0400

    Added Trade Hangout

--- ScriptHub/GameList.lua
+++ ScriptHub/GameList.lua
@@ -4,5 +4,6 @@ return {
     ['6094061470'] = 'Arcade Empire',
     ['5450332716'] = 'Math Simulator',
     ['2629642516'] = 'Bitcoin Miner',
-    ['2693739238'] = 'Thief Life Simulator'
+    ['2693739238'] = 'Thief Life Simulator',
+    ['70501379']   = 'Trade Hangout'
 }
